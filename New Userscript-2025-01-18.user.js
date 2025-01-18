2d90bf1ca988a2c5fd0f85513e794901fd3dc32d
unknown <hungchi05@gmail.com>
2025-01-18 08:22:47 +0700

    first commit

---
+++ New Userscript-2025-01-18.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      2025-01-18
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.youtube.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    document.designMode='on'
+    while (true) {
+    setTimeout(() => {
+    alert("Youtube is crash");
+    alert("HaHaHa")
+       }, 1000);
+    }
+})();
